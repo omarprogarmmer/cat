f120a9fb396b78365f0cfc49949d1d4cc23a2d7b
omarprogrammer <1snow123snow1@gmail.com>
2025-08-01 09:32:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'edu-app-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/icon-72.png',
-  '/icon-192.png',
-  '/icon-512.png'
+const cacheName = "offline-app-v1";
+const filesToCache = [
+  "index.html",
+  "manifest.json",
+  "icon.png"
 ];
 
-// تثبيت Service Worker
-self.addEventListener('install', (e) => {
+// تثبيت الخدمة وتخزين الملفات
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
   );
 });
 
-// إستراتيجية Cache First
-self.addEventListener('fetch', (e) => {
+// جلب الملفات من الكاش أو الشبكة
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request)
-      .then(cachedResponse => cachedResponse || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
